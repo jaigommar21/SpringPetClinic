b51ec589ad78436fb670718bf578b993839f7739
usuario01 <usuario01@gmail.com>
2021-04-22 19:24:24 -0500

    First version

---
+++ src/main/java/pe/edu/tecsup/springpetclinic/controllers/PetController.java
@@ -0,0 +1,19 @@
+package pe.edu.tecsup.springpetclinic.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PetController {
+
+	@GetMapping("welcome")
+	String welcome() {
+		return "Welcome to Tecsup";
+	}
+	
+	//
+	//    "inicio"
+	//    "Semana de Inicio de Clases"
+	// 
+	
+}
